656dd1eb755fed247c3345e8cac570eea7743422
ScaredSlipper <8361517@gmail.com>
2026-07-27 16:26:11 +0300

    add format option, arguments, action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,10 @@ import { program } from 'commander'
 program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
+    .helpOption('-h, --help', 'output usage information')
+    .option('-f, --format <type>', 'output format')
+    .arguments('<filepath1> <filepath2>')
+    .action(genDiff)
 
 program.parse()
 
