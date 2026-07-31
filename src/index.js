87ce999cc1f8b6c682866f0c618ba06fdae3a9a2
ScaredSlipper <8361517@gmail.com>
2026-07-31 23:25:29 +0300

    fix sonarfound problems

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ const genDiff = (filePath1, filePath2, /*format = ''*/) => {
   const file1Keys = Object.keys(file1)
   const file2Keys = Object.keys(file2)
   const diff = file1Keys
-    .filter(key1 => !file2Keys.find(key2 => key1 === key2))
+    .filter(key1 => !file2Keys.some(key2 => key1 === key2))
     .concat(file2Keys)
-    .sort()
+    .sort((a,b) => a.localeCompare(b))
     .reduce((acc, key) => {
       if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
         acc.push({ key: key, file1Value: file1[key], file2Value: file2[key] })
@@ -31,8 +31,7 @@ const genDiff = (filePath1, filePath2, /*format = ''*/) => {
         acc.push(`    ${difference.key}: ${difference.file1Value}`)
         return acc
       }
-      acc.push(`  - ${difference.key}: ${difference.file1Value}`)
-      acc.push(`  + ${difference.key}: ${difference.file2Value}`)
+      acc.push(`  - ${difference.key}: ${difference.file1Value}`, `  + ${difference.key}: ${difference.file2Value}`)
       return acc
     }
     if (Object.hasOwn(difference, 'file1Value')) {
